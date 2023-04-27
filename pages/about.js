d4646415792af67d4ed576567fe60d88144e717d
Justin Thomas <justinthomas@Shelbys-MBP-2.lan>
2023-04-26 21:31:01 -0700

    fix hover state for sector link on safari

--- pages/about.js
+++ pages/about.js
@@ -8,9 +8,6 @@ const About = () => {
             <Head>
                 <title>SPDPRK | About</title>
                 <meta name="keywords" content="about" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
             </Head>
             <main>
                 <div className={styles.about}>
