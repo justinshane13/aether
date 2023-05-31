ca22330834065374dfb409660baaa4326b0c4c87
Justin Thomas <justinshane13@gmail.com>
2023-05-30 23:02:06 -0700

    reformat and add updated content to About page

--- pages/about.js
+++ pages/about.js
@@ -14,6 +14,42 @@ const About = () => {
             </Head>
             <main>
                 <div className={styles.about}>
+                    <div className={styles.aboutBannerHorizontal}>
+                        <p>COMPATIBILITY</p>
+                        <div></div>
+                        <p>LONGEVITY</p>
+                        <div></div>
+                        <p>TOTAL SOLUTIONS</p>
+                        <div></div>
+                        <p>HARDWARE & SOFTWARE</p>
+                        <div></div>
+                        <p>ZERO OBSOLESCENCE</p>
+                    </div>
+                    <div className={styles.aboutBannerVertical}>
+                        <p>BESPOKE SOLUTIONS</p>
+                        <div></div>
+                        <p>ZERO OBSOLESCENCE</p>
+                        <div></div>
+                        <p>USA BASED</p>
+                        <div></div>
+                        <p>SERVICE</p>
+                        <div></div>
+                        <p>ZERO OBSOLESCENCE</p>
+                    </div>
+                    <motion.div
+                        initial={{ opacity: 0, y: 100 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        viewport={{ once: true, margin: "0px 0px -100px 0px" }}
+                        transition={{ duration: .5 }}
+                    >
+                        <div className={styles.aboutSection0}>
+                            <h1>Quality. Innovation.<br/>& Experience.</h1>
+                            <div>
+                                <p>Find out what separates us from<br/>the rest of the pack.</p>
+                                <Image className={styles.greenArrow} src="/green_arrow.svg" width={75} height={75} />
+                            </div>
+                        </div>
+                    </motion.div>
                     <motion.div
                         initial={{ opacity: 0, y: 100 }}
                         whileInView={{ opacity: 1, y: 0 }}
@@ -22,10 +58,10 @@ const About = () => {
                     >
                         <div className={styles.aboutSection1}>
                             <h1>How are we different?</h1>
-                            <p>We were one of the first companies to introduce a computer into the parking industry. Despite skepticism from others in the industry, we were committed to creating the best possible parking solution on the market. We know that innovation and creativity are not only relevant in our industry, but necessary.</p>
-                            <p>A favorite part of our work is tackling the complex challenges our customers face. We are only satisfied when we find a solution that works perfect for you. Work with our local partner or choose one of your own that you already enjoy — we can work with anyone. Here are some reasons you’ll be happy you chose our products.</p>
-                            <div className={styles.background1}></div>
-                            <div className={styles.background2}></div>
+                            <p>Welcome to SPDPRK, your trusted Total Solution Provider! At SPDPRK, we pride ourselves on our commitment to delivering cutting-edge technology that is built to last. With a strong emphasis on Zero Obsolescence, we ensure that both our software and hardware solutions stand the test of time, providing our clients with long-term reliability and value.</p>
+                            <p>As a Total Solution Provider, we offer a comprehensive range of services tailored to meet your every need. Whether you're looking for state-of-the-art software development, advanced hardware solutions, or a combination of both, our team of experts is here to deliver innovative and efficient solutions that drive your business forward.</p>
+                            <p>One of our key differentiators is our unwavering dedication to backwards compatibility. We understand the importance of seamless integration with existing systems, and our solutions are designed to work harmoniously with your current infrastructure. By prioritizing compatibility, we minimize disruptions and ensure a smooth transition to our advanced technologies without compromising your existing investments.</p>
+                            <div className={styles.background4}></div>
                         </div>
                     </motion.div>
                     <motion.div
@@ -35,34 +71,13 @@ const About = () => {
                         transition={{ duration: .5 }}
                     >
                         <div className={styles.aboutSection2}>
-                            <h2>We are focused on creating the highest quality user experience</h2>
                             <div>
-                                <div className={styles.aboutSection2GridElement}>
-                                    <div className={styles.aboutSection2ImageContainer}>
-                                        <Image className={styles.aboutSection2Icon} src="/handshake.svg" width={60} height={60} />
-                                    </div>
-                                    <p>Keep your local partner, we work with anyone</p>
-                                </div>
-                                <div className={styles.aboutSection2GridElement}>
-                                    <div className={styles.aboutSection2ImageContainer}>
-                                        <Image className={styles.aboutSection2Icon} src="/efficient.svg" width={70} height={70} />
-                                    </div>
-                                    <p>Designed for fast and efficient ingress and egress</p>
-                                </div>
-                                <div className={styles.aboutSection2GridElement}>
-                                    <div className={styles.aboutSection2ImageContainer}>
-                                        <Image className={styles.aboutSection2IconBottom} src="/flexible.svg" width={50} height={50} />
-                                    </div>
-                                    <p>Set up flexible payment systems that work for you</p>
-                                </div>
-                                <div className={styles.aboutSection2GridElement}>
-                                    <div className={styles.aboutSection2ImageContainer}>
-                                        <Image className={styles.aboutSection2IconBottom} src="/phone.svg" width={60} height={60} />
-                                    </div>
-                                    <p>Speak with a real person — our support staff are all in the USA</p>
-                                </div>
+                                <h2>We are focused on creating the highest quality user experience</h2>
+                                <p>We believe that customer satisfaction goes beyond delivering exceptional products. Our knowledgeable and experienced  support team is readily available to assist you with any technical inquiries, troubleshooting, or guidance you may require. We believe in fostering strong partnerships with our clients, and our internal support ensures that you receive prompt and reliable assistance.</p>
                             </div>
+                            <Image className={styles.aboutSectionImage} src="/family.jpg" width={400} height={400} />
                         </div>
+
                     </motion.div>
                     <motion.div
                         initial={{ opacity: 0, y: 100 }}
@@ -71,17 +86,13 @@ const About = () => {
                         transition={{ duration: .5 }}
                     >
                         <div className={styles.aboutSection3}>
-                            <Image className={styles.aboutSectionImage} src="/factory1.jpg" width={400} height={400} />
+                            <div className={styles.aboutSectionImageContainer}>
+                                <Image className={styles.aboutSectionImage} src="/factory1.jpg" width={400} height={400} />
+                            </div>
                             <div className={styles.aboutSection3Text}>
                                 <h2>Our equipment must meet a standard of excellence</h2>
-                                <ul>
-                                    <li>We use durable, long-lasting stainless steel</li>
-                                    <li>All configurations are made-to-order because we know every organization is different</li>
-                                    <li>Each piece of equipment is made in our Gardena CA warehouse to ensure the quality of every order</li>
-                                    <li>Pieces are assembled by hand with first-class attention to detail</li>
-                                </ul>
+                                <p>In addition to our unwavering commitment to quality and support, we take great pride in manufacturing and developing all of our equipment and software in-house, right here in the US headquarters. By maintaining complete control over our production process, we guarantee the highest standards of craftsmanship and reliability in every solution we deliver. Our dedication to domestic manufacturing also enables us to provide efficient turnaround times, ensuring that you receive your customized solutions promptly.</p>
                             </div>
-                            <div className={styles.background7}></div>
                         </div>
                     </motion.div>
                     <motion.div
@@ -92,8 +103,8 @@ const About = () => {
                     >
                         <div className={styles.aboutSection4}>
                             <h2>In addition to a high-quality product, you get peace of mind</h2>
-                            <p>Requirements change as needs and technology evolve. Secom offers long-term solutions for your investment. We have a built-in upgrade path for all systems, advanced replacement and a zero obsolescence policy on everything we produce for the life of the system.</p>
-                            <p>Secom's audit trail is unsurpassed. Reporting of all lane activities are stored on a secure server for review. All events are recorded, from video and buttons pushed to equipment action, nothing is omitted or neglected, so you are always know what happens in your garage.</p>
+                            <p>Whether you're a small parking operation or a large complex of garages, SPDPRK is here to power you with the latest technology and comprehensive solutions that will drive a successful operation.</p>
+                            <p>Experience the difference of working with a company that values longevity, compatibility, support, and domestic manufacturing.</p>
                             <div className={styles.background3}></div>
                         </div>
                     </motion.div>
